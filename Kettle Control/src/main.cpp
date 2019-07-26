d451b83a5444eb0d72aeddeb9f8a02ceaf7a3a11
Brandon <brandon2255p@gmail.com>
2019-07-26 21:58:39 +0200

    PID control loop

--- Kettle Control/src/main.cpp
+++ Kettle Control/src/main.cpp
@@ -32,17 +32,18 @@ bool masterOverride = true;
 OneWire sensorBus(Pin_DHT);
 DallasTemperature sensors(&sensorBus);
 DeviceAddress insideThermometer;
-double Setpoint, Input, Output;
-double consKp=0.2, consKi=2, consKd=0;
-PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
-int WindowSize = 500;
-unsigned long windowStartTime;
+uint pwmDutyOn, pwmDutyOff = 2000;
+double Setpoint, CurrentTemp, PreviousTemp, Output;
+double consKp=0.05, consKi=0, consKd=1;
+PID myPID(&CurrentTemp, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+uint WindowSize = 500;
+bool bIsOn = false;
+unsigned long windowStartTime, pwm_time;
 void setup() {
-    windowStartTime=millis();
+    pwm_time = millis();
     Serial.begin(9600);
     Serial.println("Booting");
     Serial.println(ssid);
-    windowStartTime=millis();
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -66,45 +67,81 @@ void setup() {
 	Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
 	Debug.showColors(true); // Color
     Debug.setCallBackProjectCmds(&processCmdRemoteDebug);
-
     sensors.begin();
     if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0");
     sensors.setResolution(9);
-    Setpoint = 30;
-    myPID.SetOutputLimits(0, WindowSize);
-    myPID.SetSampleTime(WindowSize);
+    Setpoint = 110;
+    myPID.SetOutputLimits(0, 250);
+    myPID.SetSampleTime(5000);
     myPID.SetMode(AUTOMATIC);
     pinMode(Pin_Relay_1, OUTPUT);
+    pwmDutyOn = 100;
+
+    httpServer.on("/", HTTP_GET, [&](){
+      httpServer.send(200, "text/html", "<html><head></head><body><a href='/update'>update</a></body></html>");
+    });
+    windowStartTime=millis();
+}
+
+bool handleSafetyFailure()
+{
+    if(CurrentTemp > 127)
+    {
+        CurrentTemp = 127;
+        debugE("Overtemp!!!");
+    }
+    if(CurrentTemp < 5)
+    {
+        masterOverride = true;
+        debugE("undertemp!!!");
+    }
+    if(CurrentTemp - PreviousTemp > 30)
+    {
+        masterOverride = true;
+        debugE("Large Temp Diff!!!");
+    }
+    if(masterOverride)
+    {
+        digitalWrite(Pin_Relay_1, LOW);
+        debugW("Overrideing");
+        return true;
+    }
+    return false;
 }
 
 void loop() {
     httpServer.handleClient();
     MDNS.update();
     Debug.handle();
-
-    sensors.requestTemperatures();
-    Input = sensors.getTempC(insideThermometer);
-    debugV("%0.2f vs %0.2f",Input, Setpoint);
-    if (millis() - windowStartTime > WindowSize)
+    if(millis() - windowStartTime >  WindowSize)
     {
-        auto res = myPID.Compute();
-        windowStartTime += WindowSize;
+        windowStartTime=millis();
+        sensors.requestTemperatures();
+        PreviousTemp = CurrentTemp;
+        CurrentTemp = sensors.getTempC(insideThermometer);
+        CurrentTemp = CurrentTemp > 127 ? 127.5 : CurrentTemp;
+        debugI("%0.2f# was %0.2f now %0.2f", Setpoint, PreviousTemp, CurrentTemp);
+        myPID.Compute();
+        pwmDutyOn = (uint) Output;
+        debugV("p %0.2f i %0.2f d %0.2f output %d", myPID.GetKp(), myPID.GetKi(), myPID.GetKd(), pwmDutyOn);
     }
-    if(masterOverride)
+    if(handleSafetyFailure()) return;
+    long diff = (long)(pwm_time - millis());
+    if (bIsOn && (diff <= 0))
     {
-        debugV("Overrideing");
+        bIsOn = false;
+        pwm_time = millis() + pwmDutyOff;
+        debugI("OFF %d", pwm_time);
         digitalWrite(Pin_Relay_1, LOW);
-        return;
     }
-    if (Output > 2)
+    diff = pwm_time - millis();
+    if (!bIsOn && (diff <= 0) && pwmDutyOn > 20)
     {
+        bIsOn = true;
+        pwm_time = millis() + pwmDutyOn;
+        debugI("ON %d", pwm_time);
         digitalWrite(Pin_Relay_1, HIGH);
     }
-    else
-    {
-        digitalWrite(Pin_Relay_1, LOW);
-    }
-    debugV("On %d",Output > 1);
 }
 
 void processCmdRemoteDebug() {
@@ -123,5 +160,40 @@ void processCmdRemoteDebug() {
     {
         auto sp=lastCmd.substring(2);
         Setpoint = sp.toDouble();
+    }
+    if (lastCmd.startsWith("pidp"))
+    {
+        auto sp=lastCmd.substring(4);
+        consKp = sp.toDouble();
+        myPID.SetTunings(consKp, consKi, consKd);
+    }
+    if (lastCmd.startsWith("pidi"))
+    {
+        auto sp=lastCmd.substring(4);
+        consKi = sp.toDouble();
+        myPID.SetTunings(consKp, consKi, consKd);
+    }
+    if (lastCmd.startsWith("pidd"))
+    {
+        auto sp=lastCmd.substring(4);
+        consKd = sp.toDouble();
+        myPID.SetTunings(consKp, consKi, consKd);
+    }
+    if (lastCmd.startsWith("limit"))
+    {
+        auto sp=lastCmd.substring(5);
+        auto limit = sp.toDouble();
+        limit = limit > 400 ? 400 : limit;
+        myPID.SetOutputLimits(0, limit);
+    }
+    if (lastCmd.startsWith("pwm"))
+    {
+        auto sp=lastCmd.substring(3);
+        pwmDutyOn = sp.toInt();
+        if(pwmDutyOn > pwmDutyOff){
+            debugW("Duty cycle too high %d", pwmDutyOn);
+            pwmDutyOn /= 2;
+        } 
+        debugI("On time %d", pwmDutyOn);
     }
 }
